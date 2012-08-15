0053ff2cd4727f446fb00203184a88662be222cd
Francis Devereux <francis@bright-interactive.co.uk>
2012-08-15 18:42:11 +0100

    Don't import feincms_cleanse from setup.py
    This is to fix the following error when installing:
    Traceback (most recent call last):
      File "setup.py", line 10, in <module>
        version=__import__('feincms_cleanse').__version__,
      File "/Users/francis/Code/Python/feincms-cleanse/feincms_cleanse/__init__.py", line 4, in <module>
        from BeautifulSoup import BeautifulSoup
    ImportError: No module named BeautifulSoup

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import re
 import os
 import setuplib
 
 packages, package_data = setuplib.find_packages('feincms_cleanse')
 
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("^__version__ = ['\"]([^'\"]+)['\"]", init_py, re.MULTILINE).group(1)
+
 setup(name='feincms-cleanse',
-    version=__import__('feincms_cleanse').__version__,
+    version=get_version('feincms_cleanse'),
     description='Default HTML cleansing in FeinCMS',
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
     author='Matthias Kestenholz',
